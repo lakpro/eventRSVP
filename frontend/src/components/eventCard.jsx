db87ece8f663164b22045948c19aefcd3a6e314b
Lakshay Setia <lakshaysetia123@gmail.com>
2025-05-10 19:43:46 +0530

    added throttling to prevent DoS

--- frontend/src/components/eventCard.jsx
+++ frontend/src/components/eventCard.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EventCard = ({ event, onRSVP, onUnRSVP, isRSVPed }) => {
-  // to convert 12-03-2025 to 12th March 2025
+  // Throttling helps prevent DoS attacks
+  const [lastClickTime, setLastClickTime] = useState(0);
+  const COOLDOWN = 1000; // 1 seconds
+
+  const handleClick = (action) => {
+    const now = Date.now();
+    if (now - lastClickTime < COOLDOWN) return; // throttle active
+    setLastClickTime(now); // update timestamp
+    action(event.id);
+  };
+
   function formatDate(dateString) {
     const date = new Date(dateString);
-
     const day = date.getDate();
     const year = date.getFullYear();
     const month = date.toLocaleString("en-GB", { month: "long" });
-
     const getTextDate = (n) => {
       if (n > 3 && n < 21) return "th";
       switch (n % 10) {
@@ -22,7 +30,6 @@ const EventCard = ({ event, onRSVP, onUnRSVP, isRSVPed }) => {
           return "th";
       }
     };
-
     return `${day}${getTextDate(day)} ${month} ${year}`;
   }
 
@@ -35,17 +42,15 @@ const EventCard = ({ event, onRSVP, onUnRSVP, isRSVPed }) => {
       <p className="text-gray-700 mb-4">{event.description}</p>
       <div className="flex gap-4">
         {isRSVPed ? (
-          <>
-            <button
-              onClick={() => onUnRSVP(event.id)}
-              className="bg-red-500 flex-1 text-white px-4 py-2  rounded hover:bg-red-600 cursor-pointer"
-            >
-              Un-RSVP
-            </button>
-          </>
+          <button
+            onClick={() => handleClick(onUnRSVP)}
+            className="bg-red-500 flex-1 text-white px-4 py-2 rounded hover:bg-red-600 cursor-pointer"
+          >
+            Un-RSVP
+          </button>
         ) : (
           <button
-            onClick={() => onRSVP(event.id)}
+            onClick={() => handleClick(onRSVP)}
             className="bg-green-500 text-white flex-1 px-4 py-2 rounded hover:bg-green-600 cursor-pointer"
           >
             RSVP
